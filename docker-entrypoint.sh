5d8cec13b0cb49a458a64c2de1c76af018b235f1
sax <s.apostolico@gmail.com>
2018-07-05 14:38:34 +0200

    simplify docker image and reduce size

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,8 @@
-#!/bin/bash -e
-cp /nginx.conf /etc/nginx/conf.d/default.conf
-exec "$@"
+#!/bin/sh -e
+#cp /nginx.conf /etc/nginx/conf.d/default.conf
+
+if [ "$1" == "nginx" ]; then
+    nginx -g 'daemon off;' ${NX_CONFIG}
+else
+    exec "$@"
+fi
